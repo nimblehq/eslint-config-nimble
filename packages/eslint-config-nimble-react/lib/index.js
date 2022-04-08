d893b8846bb1df410649fae21756d872292c68e2
Hoang Mirs <hoang.mirs@gmail.com>
2022-04-08 14:41:34 +0700

    [#19] Add testing-library plugin

--- packages/eslint-config-nimble-react/lib/index.js
+++ packages/eslint-config-nimble-react/lib/index.js
@@ -9,6 +9,7 @@ module.exports = {
     './rules/base',
     './rules/jsx-a11y',
     './rules/react',
+    './rules/testing',
     './rules/prettier', // prettiter must be the last one
   ].map(require.resolve),
   rules: {},
